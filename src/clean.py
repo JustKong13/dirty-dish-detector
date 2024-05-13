ae02b666724ca75d9908fad241cd903033a40fb9
Brian La <brianyla@gmail.com>
2024-05-13 16:39:56 -0400

    Cleaned data

--- src/clean.py
+++ src/clean.py
@@ -5,27 +5,17 @@ from os.path import isfile, join
 import os
 
 
-imgs = [f for f in listdir("data/clean_sink") if isfile(join("data/clean_sink", f))]
+imgs = [f for f in listdir("data/dirty_sink") if isfile(join("data/dirty_sink", f))]
 print(imgs)
 
-jpg = 0
-webp = 0
-png = 0
+num = 0
 
-for img in imgs:
-    # rewrite file name to be dirty-x
-    if img.endswith(".jpg"):
-        os.rename(
-            "data/clean_sink/" + img, "data/clean_sink/" + "clean-" + jpg + ".jpg"
-        )
-        jpg += 1
-    elif img.endswith(".webp"):
-        os.rename(
-            "data/clean_sink/" + img, "data/clean_sink/" + "clean-" + jpg + ".webp"
-        )
-        webp += 1
-    elif img.endswith(".png"):
-        os.rename(
-            "data/clean_sink/" + img, "data/clean_sink/" + "clean-" + jpg + ".png"
-        )
-        png += 1
+extensions = [".jpg", ".webp", ".png"]
+
+for ext in extensions:
+    for img in imgs:
+        if img.endswith(ext):
+            os.rename(
+                "data/dirty_sink/" + img, "data/dirty_sink/" + "dirty-" + str(num) + ext
+            )
+            num += 1
