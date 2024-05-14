bef81da4bf0488c08d01362ba6f835c713360b5d
Brian La <brianyla@gmail.com>
2024-05-13 20:18:33 -0400

    Capture image

--- src/yolo/video_feed.py
+++ src/yolo/video_feed.py
@@ -11,12 +11,12 @@ windowName = "Sink Detector"
 
 while True:
     ret, frame = video.read()
-    bbox, label, conf = cv.detect_common_objects(frame, model="yolov4-tiny")
-    output_image = draw_bbox(
-        frame, bbox, label, confidence=conf
-    )  # why isnt confidence percentage showing up ugh
 
     cv2.imshow(windowName, frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("c"):
+        print("capture")
+        cv2.imwrite("frame.jpg", frame)
 
     # Closes the window if you press q
     if cv2.waitKey(1) & 0xFF == ord("q"):
