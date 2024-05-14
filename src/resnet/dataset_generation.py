fix augmentation issue, validation computation

src/resnet/dataset_generation.py
@@ -22,30 +22,31 @@ def load_data(folder_path):
 
     for filename in os.listdir(folder_path): 
         og_img = Image.open(os.path.join(folder_path, filename))
-
+    
         img = og_img.resize((256, 256))
         img = img.convert('RGB')
-        img_tensor = torch.tensor(np.array(img) / 255.0, dtype=torch.float32) 
-        img_tensor = img_tensor.permute(2, 0, 1) 
+        img = np.array(img) / 255.0
 
-        images.append(img_tensor.numpy())
+        images.append(img)
 
         if folder_path == CLEAN_SINK_DIR: 
             labels.append(1)
         else: 
             labels.append(0)
 
-        print(img_tensor.shape)
+        img_tensor = torch.tensor(np.array(img), dtype=torch.float32) 
+        img_tensor = img_tensor.permute(2, 0, 1) 
 
         for transform in train_transforms: 
-            transformed_img = transform(img_tensor)
-            images.append(transformed_img.numpy())
+            transformed_img = transform(img_tensor) # result of transform is 3 x h x w
+            channel_last_img = transformed_img.permute(1, 2, 0)
+            images.append(channel_last_img.numpy())
             if folder_path == CLEAN_SINK_DIR: 
                 labels.append(1)
             else: 
                 labels.append(0)
 
-    return np.stack(images), np.array(labels)
+    return np.array(images), np.array(labels)
 
 
 
@@ -56,7 +57,7 @@ X = np.concatenate((clean_images, dirty_images))
 y = np.concatenate((clean_labels, dirty_labels))
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 1738)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1738)
 
 
 class SinkDataset(Dataset):

src/resnet/main.py
@@ -3,8 +3,8 @@ from train import *
 from dataset_generation import * 
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=2e-4)
-epochs = 7
+optimizer = optim.Adam(model.parameters(), lr=2e-2)
+epochs = 4
 train_loss, val_loss, pred, result_model = train(model, 
                                                  train_loader, 
                                                  test_loader, 

src/resnet/train.py
@@ -9,20 +9,23 @@ import copy
 
 
 def val(model, val_data_loader, criterion):
+    model.eval()
     val_running_loss = 0
-    accuracy = 0
-    counter = 0
+    total_correct = 0
+    total_samples = 0
+
     with torch.no_grad():
-        for i, (inputs, labels) in enumerate(val_data_loader, 0):
+        for inputs, labels in val_data_loader:
             outputs = model(inputs)
             loss = criterion(outputs, labels.to(torch.long))
             val_running_loss += loss.item()
-            output_decisions = torch.argmax(outputs, dim=1)
-            accuracy = torch.sum(output_decisions == labels)
-            counter += 1 
-    accuracy = accuracy / counter
+            predictions = outputs.argmax(dim=1, keepdim=True)
+            total_correct += predictions.eq(labels.view_as(predictions)).sum().item()
+            total_samples += labels.size(0)
 
-    return val_running_loss, accuracy
+    accuracy = total_correct / total_samples
+
+    return val_running_loss / len(val_data_loader), accuracy
 
 def train(model, data_loader, val_data_loader, criterion, optimizer, epochs):
     train_loss_arr = []