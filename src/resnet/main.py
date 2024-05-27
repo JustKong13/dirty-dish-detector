1f01d5f1240610ac264a0df0fbcd9244a20e14ab
rgu0114 <richardgu0114@gmail.com>
2024-05-26 23:41:20 -0700

    fc2 bn2 ++ expts

--- src/resnet/main.py
+++ src/resnet/main.py
@@ -3,9 +3,9 @@ from train import *
 from dataset_generation import * 
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=1e-2, weight_decay=1e-5)
+optimizer = optim.Adam(model.parameters(), lr=5e-3, weight_decay=1e-5)
 scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
-epochs = 2
+epochs = 8
 train_loss, val_loss, train_pred, pred, result_model = train(model, 
                                                  train_loader, 
                                                  test_loader, 
@@ -21,4 +21,4 @@ print('validation accuracy', pred)
 
 
 # pkl.dump(result_model, open('unbias_tuned_model_resnet.pkl', 'wb'))
-torch.save(result_model.state_dict(), './models/v4_unbias_tuned_model_resnet.pt')
+torch.save(result_model.state_dict(), './models/v8_unbias_tuned_model_resnet.pt')
--- src/resnet/model.py
+++ src/resnet/model.py
@@ -13,23 +13,34 @@ class ResnetArchitecture(nn.Module):
         for param in self.resnet.parameters():
             param.requires_grad = False
 
-        for param in self.resnet.layer4.parameters():
-            param.requires_grad = True
+        # for param in self.resnet.layer4.parameters():
+        #     param.requires_grad = True
             
         self.fc1 = nn.Linear(1000, 512)
         self.bn1 = nn.BatchNorm1d(512)
-        self.relu = nn.ReLU()
+        self.relu1 = nn.ReLU()
+
+        self.fc2 = nn.Linear(512, 256)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.relu2 = nn.ReLU()
+
         self.dropout = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(512, 2)
+        self.fc3 = nn.Linear(256, 2)
 
     def forward(self, x):
         x = self.resnet(x)
-        x = x.view(x.size(0), -1)
+        # x = x.view(x.size(0), -1)
         x = self.fc1(x)
         x = self.bn1(x)
-        x = self.relu(x)
-        x = self.dropout(x)
+        x = self.relu1(x)
+
         x = self.fc2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+
+        x = self.dropout(x)
+
+        x = self.fc3(x)
         
         return x
     
